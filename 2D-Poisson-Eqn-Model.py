30274ac6486391ba92c0144b7836ae59cad362aa
Josh Wiggs <josh.wiggs@outlook.com>
2018-11-20 15:24:51 +0000

    Included pseudo-time evolution in p

--- 2D-Poisson-Eqn-Model.py
+++ 2D-Poisson-Eqn-Model.py
@@ -18,10 +18,10 @@ dy = (y_max - y_min) / (ny-1)
 #Create arrays
 x = np.zeros(nx)
 y = np.zeros(nx)
-p = np.zeros((nx,ny))
+p = np.zeros((nt,nx,ny))
 p_temp = np.zeros((nx,ny))
-p_initial = np.zeros((nx,ny))
-inp = np.zeros_like(p) #driver
+p_i = np.zeros((nx,ny))
+inp = np.zeros_like(p_temp) #driver
 
 #Populate dimensional vectors
 x = np.linspace(x_min,x_max,num=nx)
@@ -31,31 +31,34 @@ y = np.linspace(y_min,y_max,num=ny)
 inp[int(nx / 4), int(ny / 4)]  = 100
 inp[int(3 * nx / 4), int(3 * ny / 4)] = -100
 
-for t in range(nt):
-    
+for t in range(nt):    #Pseudo-time
+
     if t % 10 == 0:
         print(t)
-        
+
     for i in range(1,nx-1):
         for j in range(1,ny-1):
 
-            p_temp = p.copy()
+            p_temp = p_i.copy()
 
-            p[i][j] = ((p_temp[i + 1][j] + p_temp[i - 1][j]) * (dy ** 2) +
+            p_i[i][j] = ((p_temp[i + 1][j] + p_temp[i - 1][j]) * (dy ** 2) +
             (p_temp[i][j + 1] + p_temp[i][j - 1]) * (dx ** 2) - (inp[i][j] *
             (dx ** 2) * (dy ** 2))) / (2 * ((dx ** 2) + (dy ** 2)))
 
-            p[0, :] = 0
-            p[ny-1, :] = 0
-            p[:, 0] = 0
-            p[:, nx-1] = 0
+            #BC's
+            p_i[0, :] = 0
+            p_i[ny-1, :] = 0
+            p_i[:, 0] = 0
+            p_i[:, nx-1] = 0
+
+            p[t] = p_i.copy()
 
 #Meshgrid
 X, Y = np.meshgrid(x, y)
 
 #Plotting - 3D with z = velocity domain
-def sur_plot(r):
+def time_plot(r,t):
     fig = plt.figure(figsize=(11, 7), dpi=100)
     ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(X, Y, r, cmap=cm.viridis)
+    surf = ax.plot_surface(X, Y, r[t], cmap=cm.viridis)
     plt.show()
