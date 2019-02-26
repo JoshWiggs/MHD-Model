192fcf51a10f92a4c2fd6aa5280a566ab7ec27f5
Josh Wiggs <josh.wiggs@outlook.com>
2019-02-26 17:25:22 +0000

    Magnetic field generation / numerical method broken
    In this version of the code the magnetic field is either not generating correctly or is not being correctly utilized by the numerical technique progressing the plasma through time.

--- 2D-MHD-Model.py
+++ 2D-MHD-Model.py
@@ -13,10 +13,12 @@ class options:
         self.IncludeDiffusion = bool(True)
         self.IncludeMagnetism = bool(False)
         self.CalculatePressure = bool(False)
-        self.CalculateVelocityMagnitude = bool(False)
+        self.CalculateVelocityMagnitude = bool(True)
+        self.FTCSMethod = bool(False) #Select either this or Leap Frog method
+        self.LeapFrogMethod = bool(True) #if using this ensure diffusion is on
 
         #Advanced Options
-        self.TemporalSparseStorage = bool(False)
+        self.TemporalSparseStorage = bool(False) # not compatible with leapfrog
         #TODO: add sparse storage modes
 
     class setup:
@@ -27,13 +29,13 @@ class options:
             self.y_min = -2
             self.y_max = 2
             self.t_initial = 0
-            self.t_end = 1
-            self.viscosity = 0.1 #0.001
-            self.density = 10 #1
+            self.t_end = 2.0
+            self.viscosity = 0.1 #use 0.1 for stable FTCS
+            self.density = 1 #1
             self.magnetic_permeability = 1 #1
             self.nx = 100
             self.ny = 100
-            self.nt = 5001
+            self.nt = 2001
             self.simulation_run_time = 2001
             #TODO: add initial condition setup (maybe new class)
 
@@ -73,16 +75,15 @@ def magnetic_field_2D(nx,ny,x,y):
 
     for i in range(0,nx):
 
-        B_x[i] = 0.25
+        B_x[i] = 2 * np.abs(y[i])
 
     for j in range(0,ny):
 
-        B_y[j] = 0
+        B_y[j] = 2 * np.abs(x[j])
 
-    for i in range(0,nx):
-        for j in range(0,ny):
+    BX,BY = np.meshgrid(B_x,B_y)
 
-            B_mag[i][j] = np.sqrt(B_x[i]**2 + B_y[j]**2)
+    B_mag = np.sqrt(BX**2 + BY**2)
 
     return B_x, B_y, B_mag
 
@@ -163,9 +164,63 @@ def diffusion_2D(vis,dt,dx,dy,u_i,v_i):
 
 def magnetic_2D(vis,mu,dt,dx,dy,BX,BY,B_mag):
 
-    u_mag = (((BX * dt) / (vis * mu * dx)) * (BX - np.roll(BX,1,axis=0))) +(((BY * dt) / (vis * mu * dy)) * (BX - np.roll(BX,1,axis=1))) +((dt /(2 * vis * mu * dx)) * ((B_mag) ** 2 - (np.roll(B_mag,1,axis=0)) ** 2))
+    u_mag = (((BX * dt) / (vis * mu * dx)) * (BX - np.roll(BX,1,axis=0))) +(((BY * dt) / (vis * mu * dy)) * (BX - np.roll(BX,1,axis=1))) + ((dt /(2 * vis * mu * dx)) * ((B_mag) ** 2 - (np.roll(B_mag,1,axis=0)) ** 2))
 
     v_mag = (((BX * dt) / (vis * mu * dx)) * (BY - np.roll(BY,1,axis=0))) +(((BY * dt) / (vis * mu * dy)) * (BY - np.roll(BY,1,axis=1))) + ((dt /(2 * vis * mu * dy)) * ((B_mag) ** 2 - (np.roll(B_mag,1,axis=1)) ** 2))
+
+    # ignore the edges: since the above code will have modified the edge cells, reset them.
+    u_mag[:,0] = 0
+    u_mag[:,-1] = 0
+    u_mag[0,:] = 0
+    u_mag[-1,:] = 0
+    v_mag[:,0] = 0
+    v_mag[:,-1] = 0
+    v_mag[0,:] = 0
+    v_mag[-1,:] = 0
+
+    return u_mag, v_mag
+
+def leapfrog_convection_2D(alpha,dt,dx,dy,u_i,v_i):
+
+    u_conv = alpha * (-u_i*(np.roll(u_i,-1,axis=0)-np.roll(u_i,1,axis=0))*dt/dx - v_i*(np.roll(u_i,-1,axis=1)-np.roll(u_i,1,axis=1))*dt/dy)
+
+    v_conv = alpha * (-u_i*(np.roll(v_i,-1,axis=0)-np.roll(v_i,1,axis=0))*dt/dx - v_i*(np.roll(v_i,-1,axis=1)-np.roll(v_i,1,axis=1))*dt/dy)
+
+    # ignore the edges: since the above code will have modified the edge cells, reset them.
+    u_conv[:,0] = 0
+    u_conv[:,-1] = 0
+    u_conv[0,:] = 0
+    u_conv[-1,:] = 0
+    v_conv[:,0] = 0
+    v_conv[:,-1] = 0
+    v_conv[0,:] = 0
+    v_conv[-1,:] = 0
+
+    return u_conv, v_conv
+
+def leapfrog_diffusion_2D(alpha,vis,dt,dx,dy,u_i,v_i,u_ii,v_ii):
+
+    u_diff = alpha * ((2*vis*dt/dx**2) * (np.roll(u_i,1,axis=0)-u_ii + np.roll(u_i,-1,axis=0)) + (2*vis*dt/dy**2) * (np.roll(u_i,1,axis=1) -u_ii + np.roll(u_i,-1,axis=1)))
+
+    v_diff = alpha * ((2*vis*dt/dx**2) * (np.roll(v_i,1,axis=0)-v_ii + np.roll(v_i,-1,axis=0)) + (2*vis*dt/dy**2) * (np.roll(v_i,1,axis=1) -v_ii + np.roll(v_i,-1,axis=1)))
+
+    # ignore the edges: since the above code will have modified the edge cells, reset them.
+    u_diff[:,0] = 0
+    u_diff[:,-1] = 0
+    u_diff[0,:] = 0
+    u_diff[-1,:] = 0
+    v_diff[:,0] = 0
+    v_diff[:,-1] = 0
+    v_diff[0,:] = 0
+    v_diff[-1,:] = 0
+
+    return u_diff ,v_diff
+
+def leapfrog_magnetic_2D(alpha,vis,mu,dt,dx,dy,BX,BY,B_mag):
+
+    u_mag = alpha * ((((BX * dt) / (vis * mu * dx)) * (np.roll(BX,-1,axis=0) - np.roll(BX,1,axis=0))) +(((BY * dt) / (vis * mu * dy)) * (np.roll(BX,-1,axis=1) - np.roll(BX,1,axis=1))) +((dt /(2 * vis * mu * dx)) * ((np.roll(B_mag,-1,axis=0)) ** 2 - (np.roll(B_mag,1,axis=0)) ** 2)))
+
+    v_mag = alpha * ((((BX * dt) / (vis * mu * dx)) * (np.roll(BY,-1,axis=0) - np.roll(BY,1,axis=0))) +(((BY * dt) / (vis * mu * dy)) * (np.roll(BY,-1,axis=1) - np.roll(BY,1,axis=1))) +((dt /(2 * vis * mu * dy)) * ((np.roll(B_mag,-1,axis=1)) ** 2 - (np.roll(B_mag,1,axis=1)) ** 2)))
 
     # ignore the edges: since the above code will have modified the edge cells, reset them.
     u_mag[:,0] = 0
@@ -200,8 +255,6 @@ else:
     u = np.zeros((su.simulation_run_time,su.nx,su.ny))
     v = np.zeros((su.simulation_run_time,su.nx,su.ny))
 
-
-
 #Open variable for velocity magnitude if CalculateVelocityMagnitude option is on
 if opt.CalculateVelocityMagnitude is True:
     U = np.zeros((su.simulation_run_time,su.nx,su.ny))
@@ -212,6 +265,11 @@ if opt.CalculatePressure is True:
     v_new = np.zeros((su.nx,su.ny))
     p_nt = pres.pressure_smoothing_iterations #peudo-time for pressure
     p = np.zeros((su.simulation_run_time,su.nx,su.ny))
+
+#Open additional time variable if leapfrog method being used
+if opt.LeapFrogMethod is True:
+    u_ii = np.zeros((su.nx,su.ny)) #u^n-1 for leapfrog
+    alpha = 1 / (1 + ((2*su.viscosity*dt) * ((dx**2) + (dy**2))/((dx**2)*(dy**2))))
 
 #Populate dimensional vectors
 x = np.linspace(su.x_min,su.x_max,num=su.nx)
@@ -224,9 +282,20 @@ B_x, B_y, B_mag = magnetic_field_2D(su.nx,su.ny,x,y)
 BX,BY = np.meshgrid(B_x,B_y)
 
 #IC's
+"""
+u[0][:][:] = 0.25
+v[0][:][:] = 0.25
+
+for i in range(0,24):
+    for j in range(0,24):
+        u[0][24+i][24+j] = 2
+        v[0][24+i][24+j] = 2
+
+"""
+
 for i in range(0,int(np.round((su.nx / 2.0)))):
-     u[0][:][i] = -0.25
-     u[0][:][int((np.round(su.nx / 2.0))) + i] = 0.25
+     u[0][:][i] = -0.1
+     u[0][:][int((np.round(su.nx / 2.0))) + i] = 0.1
 
      xi = np.zeros(su.nx)
      for i in range(0,su.nx):
@@ -239,6 +308,7 @@ for i in range(0,int(np.round((su.nx / 2.0)))):
      v[0][:][int(np.floor(su.nx/2.0))] = xi
      v[0][:][int(np.floor(su.nx/2.0)) + 1] = xi
 
+
 u_i = u[0].copy()
 v_i = v[0].copy()
 
@@ -246,6 +316,7 @@ v_i = v[0].copy()
 if opt.CalculatePressure is True:
     u_new = u[0].copy()
     v_new = v[0].copy()
+
 
 #Meshgrid
 X, Y = np.meshgrid(x, y)
@@ -257,8 +328,46 @@ if opt.CalculatePressure is True:
 
     p[0] = p_i.copy()
 
+#For Leap Frog first time step needs to be done with FTCS,
+#that is done here
+if opt.LeapFrogMethod is True:
+    #If IncludeConvection option is turned on then
+    if opt.IncludeConvection is True:
+        u_conv,v_conv = convection_2D(dt,dx,dy,u_i,v_i)
+    else:
+        u_conv = 0
+        v_conv = 0
+
+    #If IncludeDiffusion option is turned on then
+    if opt.IncludeDiffusion is True:
+        u_diff,v_diff = diffusion_2D(su.viscosity,dt,dx,dy,u_i,v_i)
+    else:
+        u_diff = 0
+        v_diff = 0
+
+    #If IncludeMagnetism option is turned on then
+    if opt.IncludeMagnetism is True:
+        u_mag,v_mag = magnetic_2D(su.viscosity,su.magnetic_permeability,dt,dx,dy,BX,BY,B_mag)
+    else:
+        u_mag = 0
+        v_mag = 0
+
+    # CHRIS: VECTORISED - can just add the arrays - NumPy will do it internally.
+    u_i = u_i + u_conv + u_diff + u_mag
+    v_i = v_i + v_conv + v_diff + v_mag
+
+    u_ii = u[0].copy()
+    v_ii = v[0].copy()
+    u[1] = u_i.copy()
+    v[1] = v_i.copy()
+
 #Iterate through time over spaital (x,y) domain from IC's
-for n in range(1,su.simulation_run_time):
+if opt.LeapFrogMethod is True:
+    initial_step = int(2)
+else:
+    initial_step = int(1)
+
+for n in range(initial_step,su.simulation_run_time):
 
     # CHRIS: Check CFL limit
     cfl = np.max((u_i/dx + v_i/dy)*dt)
@@ -299,36 +408,43 @@ for n in range(1,su.simulation_run_time):
             #v_i[i][j] = Decimal(v_i[i][j])
     """
 
-    #If IncludeConvection option is turned on then call convection_2D function
-    #previously definied to calculate the convection element from the momentum
-    #eqn using Euler differencing.
+    #If IncludeConvection option is turned on then
     if opt.IncludeConvection is True:
-        u_conv,v_conv = convection_2D(dt,dx,dy,u_i,v_i)
+        if opt.FTCSMethod is True:
+            u_conv,v_conv = convection_2D(dt,dx,dy,u_i,v_i)
+        elif opt.LeapFrogMethod is True:
+            u_conv,v_conv = leapfrog_convection_2D(alpha,dt,dx,dy,u_i,v_i)
     else:
         u_conv = 0
         v_conv = 0
 
-    #If IncludeDiffusion option is turned on then call diffusion_2D function
-    #previously definied to calculate the diffusion element from the momentum
-    #eqn using center differencing.
+    #If IncludeDiffusion option is turned on then
     if opt.IncludeDiffusion is True:
-        u_diff,v_diff = diffusion_2D(su.viscosity,dt,dx,dy,u_i,v_i)
+        if opt.FTCSMethod is True:
+            u_diff,v_diff = diffusion_2D(su.viscosity,dt,dx,dy,u_i,v_i)
+        elif opt.LeapFrogMethod is True:
+            u_diff,v_diff = leapfrog_diffusion_2D(alpha,su.viscosity,dt,dx,dy,u_i,v_i,u_ii,v_ii)
     else:
         u_diff = 0
         v_diff = 0
 
-    #If IncludeMagnetism option is turned on then call magnetic_2D function
-    #previously definied to calculate the magnetism element from the momentum
-    #eqn using Euler differencing.
+    #If IncludeMagnetism option is turned on then
     if opt.IncludeMagnetism is True:
-        u_mag,v_mag = magnetic_2D(su.viscosity,su.magnetic_permeability,dt,dx,dy,BX,BY,B_mag)
+        if opt.FTCSMethod is True:
+            u_mag,v_mag = magnetic_2D(su.viscosity,su.magnetic_permeability,dt,dx,dy,BX,BY,B_mag)
+        elif opt.LeapFrogMethod is True:
+            u_mag,v_mag = leapfrog_magnetic_2D(alpha,su.viscosity,su.magnetic_permeability,dt,dx,dy,BX,BY,B_mag)
     else:
         u_mag = 0
         v_mag = 0
 
     # CHRIS: VECTORISED - can just add the arrays - NumPy will do it internally.
-    u_i = u_i + u_conv + u_diff + u_mag
-    v_i = v_i + v_conv + v_diff + v_mag
+    if opt.FTCSMethod is True:
+        u_i = u_i + u_conv + u_diff + u_mag
+        v_i = v_i + v_conv + v_diff + v_mag
+    elif opt.LeapFrogMethod is True:
+        u_i = (alpha * u_ii) + u_conv + u_diff + u_mag
+        v_i = (alpha * v_ii) + v_conv + v_diff + v_mag
 
     if opt.CalculatePressure is True:
         b,p_i = pressure_driving_force_2D(su.density,su.nx,su.ny,p_nt,dt,dx,dy,u_i,v_i)
@@ -346,7 +462,7 @@ for n in range(1,su.simulation_run_time):
             plt.title('$t$ = ' + str(n/(su.nt-1)))
             plt.streamplot(X,Y,u_i,v_i)
             plt.subplot(2,1,2)
-            plt.title('$t$ = ' + str(n/(su.nt-1)))
+            plt.title('$t$ = ' + str(t[n]))
             plt.contourf(X,Y,p_i,cmap=cm.seismic)
             plt.colorbar(label = '$p$')
             plt.draw()
@@ -357,6 +473,12 @@ for n in range(1,su.simulation_run_time):
                 u[int(n/100)] = u_new.copy()
                 v[int(n/100)] = v_new.copy()
                 p[int(n)] = p_i.copy()
+        elif opt.LeapFrogMethod is True:
+            u_ii = u[n - 1].copy()
+            v_ii = v[n - 1].copy()
+            u[n] = u_new.copy()
+            v[n] = v_new.copy()
+            p[n] = p_i.copy()
         else:
             u[n] = u_new.copy()
             v[n] = v_new.copy()
@@ -368,7 +490,7 @@ for n in range(1,su.simulation_run_time):
 
             plt.clf()
             plt.streamplot(X,Y,u_i,v_i)
-            plt.title('$t$ = ' + str(n/(su.nt-1)))
+            plt.title('$t$ = ' + str(t[n]))
             plt.draw()
             plt.pause(0.001)
 
@@ -376,7 +498,11 @@ for n in range(1,su.simulation_run_time):
             if n % 100 == 0:
                 u[int(n/100)] = u_i.copy()
                 v[int(n/100)] = v_i.copy()
-
+        elif opt.LeapFrogMethod is True:
+            u_ii = u[n - 1].copy()
+            v_ii = v[n - 1].copy()
+            u[n] = u_i.copy()
+            v[n] = v_i.copy()
         else:
             u[n] = u_i.copy()
             v[n] = v_i.copy()
